0453f5501e82682abd245207c1168b0c9053311b
userdebug23 <abhinavchandranap14@hotmail.com>
2026-07-02 16:38:58 +0530

    Create FoodTrackerApplication.kt

---
+++ app/src/main/java/com/foodtracker/FoodTrackerApplication.kt
@@ -0,0 +1,11 @@
+package com.foodtracker
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class FoodTrackerApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
