886accdd6a4d26c2eff2dc1778113ac915b0497a
userdebug23 <abhinavchandranap14@hotmail.com>
2026-07-03 17:46:42 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,11 +5,10 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.4.2")
+        classpath("com.android.tools.build:gradle:8.1.4")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.21")
     }
 }
-
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
 }
