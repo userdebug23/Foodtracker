442296a346eb3b3139bfdaf0b588f2d0e53b94f9
userdebug23 <abhinavchandranap14@hotmail.com>
2026-07-04 15:08:35 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,25 +5,6 @@ plugins {
     id("com.google.dagger.hilt.android") version "2.50" apply false
 }
 
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.2.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20")
-
-        // Hilt
-        classpath("com.google.dagger:hilt-android-gradle-plugin:2.50")
-
-        // KSP
-        classpath("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:1.9.20-1.0.14")
-    }
-}
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
 }
