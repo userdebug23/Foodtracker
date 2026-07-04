0c7e8b0a0f314e24a2cecd238af4841bd8dce196
userdebug23 <abhinavchandranap14@hotmail.com>
2026-07-04 14:54:15 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,9 +3,16 @@ buildscript {
         google()
         mavenCentral()
     }
+
     dependencies {
         classpath("com.android.tools.build:gradle:8.2.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20")
+
+        // Hilt
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.50")
+
+        // KSP
+        classpath("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:1.9.20-1.0.14")
     }
 }
 
