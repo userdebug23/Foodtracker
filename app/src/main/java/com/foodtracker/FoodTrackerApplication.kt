deea5dc674e4116df1f469f6d8251d495e3de9a3
userdebug23 <abhinavchandranap14@hotmail.com>
2026-07-04 16:57:28 +0530

    Update FoodTrackerApplication.kt

--- app/src/main/java/com/foodtracker/FoodTrackerApplication.kt
+++ app/src/main/java/com/foodtracker/FoodTrackerApplication.kt
@@ -2,7 +2,6 @@ package com.foodtracker
 
 import android.app.Application
 
-// REMOVE @HiltAndroidApp for now to avoid Hilt issues
 class FoodTrackerApplication : Application() {
     override fun onCreate() {
         super.onCreate()
