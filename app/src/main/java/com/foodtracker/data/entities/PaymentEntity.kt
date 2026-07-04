c3ad0613b18e75a837662207f7e24e22f16fc667
userdebug23 <abhinavchandranap14@hotmail.com>
2026-07-04 18:50:25 +0530

    Create PaymentEntity.kt

---
+++ app/src/main/java/com/foodtracker/data/entities/PaymentEntity.kt
@@ -0,0 +1,27 @@
+package com.foodtracker.data.entities
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.time.LocalDate
+
+@Entity(tableName = "payments")
+data class PaymentEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
+    
+    @ColumnInfo(name = "payment_date")
+    val paymentDate: LocalDate,
+    
+    @ColumnInfo(name = "amount")
+    val amount: Double,
+    
+    @ColumnInfo(name = "payment_method")
+    val paymentMethod: String = "Cash",
+    
+    @ColumnInfo(name = "remarks")
+    val remarks: String = "",
+    
+    @ColumnInfo(name = "created_at")
+    val createdAt: String = LocalDate.now().toString()
+)
