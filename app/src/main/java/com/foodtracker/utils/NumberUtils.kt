0f42c4fdc9273831418e82484611a18d07bf9644
userdebug23 <abhinavchandranap14@hotmail.com>
2026-07-04 17:32:06 +0530

    Create NumberUtils.kt

---
+++ app/src/main/java/com/foodtracker/utils/NumberUtils.kt
@@ -0,0 +1,28 @@
+package com.foodtracker.utils
+
+import java.text.NumberFormat
+import java.util.Locale
+
+object NumberUtils {
+    private val currencyFormat = NumberFormat.getCurrencyInstance(Locale.getDefault())
+    
+    fun formatCurrency(amount: Double): String {
+        return currencyFormat.format(amount)
+    }
+    
+    fun calculateDailyExpense(breakfast: Boolean, lunch: Boolean, dinner: Boolean, mealRate: Double = 50.0): Double {
+        var count = 0
+        if (breakfast) count++
+        if (lunch) count++
+        if (dinner) count++
+        return count * mealRate
+    }
+    
+    fun calculateMealCount(breakfast: Boolean, lunch: Boolean, dinner: Boolean): Int {
+        var count = 0
+        if (breakfast) count++
+        if (lunch) count++
+        if (dinner) count++
+        return count
+    }
+}
