12415c0c642cb2aa78b7eb02b516e8c66cb34ed3
userdebug23 <abhinavchandranap14@hotmail.com>
2026-07-05 00:00:01 +0530

    Update FoodRepository.kt

--- app/src/main/java/com/foodtracker/data/repository/FoodRepository.kt
+++ app/src/main/java/com/foodtracker/data/repository/FoodRepository.kt
@@ -4,29 +4,30 @@ import com.foodtracker.data.dao.FoodEntryDao
 import com.foodtracker.data.entities.FoodEntry
 import java.time.LocalDate
 import java.time.YearMonth
-import java.time.format.DateTimeFormatter
 
 class FoodRepository(private val dao: FoodEntryDao) {
-    
-    private val dateFormatter = DateTimeFormatter.ISO_LOCAL_DATE
     
     suspend fun saveEntry(entry: FoodEntry): Long {
         return dao.insertEntry(entry)
     }
     
     suspend fun getEntry(date: LocalDate): FoodEntry? {
-        return dao.getEntryByDate(date.format(dateFormatter))
+        return dao.getEntryByDate(date.toString())
     }
     
-    ssuspend fun getEntriesBetween(startDate: LocalDate, endDate: LocalDate): List<FoodEntry> {
-    return dao.getEntriesBetween(startDate.toString(), endDate.toString())
-}
+    suspend fun getEntriesBetween(startDate: LocalDate, endDate: LocalDate): List<FoodEntry> {
+        return dao.getEntriesBetween(startDate.toString(), endDate.toString())
+    }
+    
+    suspend fun getEntriesByMonth(yearMonth: YearMonth): List<FoodEntry> {
+        return dao.getEntriesByMonth(yearMonth.toString())
+    }
     
     suspend fun getMonthlySummary(yearMonth: YearMonth): MonthlySummary {
         val startDate = yearMonth.atDay(1)
         val endDate = yearMonth.atEndOfMonth()
-        val startStr = startDate.format(dateFormatter)
-        val endStr = endDate.format(dateFormatter)
+        val startStr = startDate.toString()
+        val endStr = endDate.toString()
         
         val entries = dao.getEntriesBetween(startStr, endStr)
         val totalDays = endDate.dayOfMonth
@@ -37,7 +38,7 @@ class FoodRepository(private val dao: FoodEntryDao) {
         val totalLunch = dao.getLunchCount(startStr, endStr)
         val totalDinner = dao.getDinnerCount(startStr, endStr)
         val totalMeals = dao.getTotalMeals(startStr, endStr)
-        val totalExpense = dao.getTotalExpense(startStr, endStr)
+        val totalExpense = dao.getTotalExpense(startStr, endStr) ?: 0.0
         val averageDailyExpense = if (totalDays > 0) totalExpense / totalDays else 0.0
         
         return MonthlySummary(
